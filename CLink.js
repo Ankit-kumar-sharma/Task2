eb8ffc69d198d985ec7ecfdadfa7c533de79c36f
Ankit Kumar Sharma <73944362+Ankit-kumar-sharma@users.noreply.github.com>
2021-07-02 10:29:00 +0530

    Add files via upload

---
+++ CLink.js
@@ -0,0 +1,8 @@
+function myFunction()
+{
+    var copyText=document.getElementById("clink");
+    copyText.select();
+    copyText.setSelectionRange(0,999999)
+    document.execCommand("copy");
+    
+}
